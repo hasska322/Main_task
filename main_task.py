fefdad642b311dd44169b7463ee0da859e1b9b2c
Andrey <hasska322@gmail.com>
2020-06-11 17:09:21 +0300

    Solution on Python

--- main_task.py
+++ main_task.py
@@ -1,76 +1,26 @@
-import requests
-from bs4 import BeautifulSoup as bs
-import re
-import lxml
+from random import randint
 
 
-def get_html(url):
-    """
-    function for getting page be requests
-    : param url: URL for request
-    : return : Page
-    """
-    #  Input your user - agent
-    #  If wanna know your user-agent for Google command "about:"
-    headers = {
-        'User-Agent': ''
-    }
-    r = requests.get(url, headers=headers)
-    r.encoding = 'utf8'
-    into_file(r.text)
-    return r.text
+n = randint(1, 100000)
+target = randint(1, n)
+print("n =", n, "target =", target)
 
 
-def into_file(text):
-    """"
-    Save page into the file
-    :param text: Page for saving
-    """
-    with open("target.html", "w", encoding="utf8") as target:
-        print(text, file=target)
-
-
-def soup_info(text):
-    """Function for parsing title's of free video on Udemy
-    : param text : Page for parsing
-    :return file: Saved file with answer"""
-    soup = bs(text, "lxml")
-    head = soup.find("div", class_="lectures-container collapse in").find_all("a", href=re.compile("javascript"),
-                                                                              class_=None)
-    heads = [i.text for i in head if i.text != "\nPreview\n"]
-    cleaner(heads)
-
-    time = soup.find("div", class_='lectures-container collapse in').find_all("span", ["preview-text",
-                                                                                       "content-summary"])
-    times = [i.text for i in time]
-    cleaner(times)
-
-    i = 0
-    while True:
-        if times[i] == "Preview" or times[i] != "Preview" and times[i+1] == "Preview":
-            i += 1
+def result():
+    res = 0
+    ideal = 0
+    for i in range(1, n + 1):
+        if i == target:
+            res += 90/i
             continue
-        elif times[i] != "Preview" and times[i+1] != "Preview":
-            del times[i+1]
-            i += 1
-        if i >= len(times):
-            break
-    times = [i for i in times if i != "Preview"]
-    answer = list(zip(heads, times))
-    answer.sort(key=lambda i: i[1])
-    with open("answer.txt", "w") as file:
-        for i in answer:
-            print(*i, file=file)
+        res += 100 / i
+
+    for i in range(1, n + 1):
+        ideal += 100 / i
+
+    return round(100 / ((ideal - res) * 10))
 
 
-def cleaner(lst):
-    for i in range(len(lst)):
-        lst[i] = lst[i].strip()
-    return lst
+print(result())
 
 
-udemy = "https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/"
-
-text_info = get_html(udemy)
-into_file(text_info)
-soup_info(text_info)
