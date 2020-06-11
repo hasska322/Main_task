fb197d3d5a1c7f413260d28827c813943b79a0bc
Andrey <hasska322@gmail.com>
2020-06-11 17:14:10 +0300

    Using Generator's

--- main_task.py
+++ main_task.py
@@ -3,22 +3,15 @@ from random import randint
 
 n = randint(1, 100000)
 target = randint(1, n)
+
 print("n =", n, "target =", target)
 
 
 def result():
-    res = 0
-    ideal = 0
-    for i in range(1, n + 1):
-        if i == target:
-            res += 90/i
-            continue
-        res += 100 / i
+    res = sum([(100 / i) if i != target else 90 / i for i in range(1, n + 1)])
+    ideal = sum([100 / i for i in range(1, n + 1)])
 
-    for i in range(1, n + 1):
-        ideal += 100 / i
-
-    return round(100 / ((ideal - res) * 10))
+    return round(10 / (ideal - res))
 
 
 print(result())
